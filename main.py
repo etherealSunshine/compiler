2759cefe906ca7f05eebdb47be4973e2fadbc1f9
etherealSunshine <aqarian77@gmail.com>
2022-02-04 10:31:00 +0530

    added string interpretation

---
+++ main.py
@@ -0,0 +1,39 @@
+class interpreter:
+    def __init__(self):
+        pass
+    def run(self,code):
+        for xs in code: 
+            self.eval(xs)
+
+    def eval(self,xs):
+        if isinstance(xs,list):
+            return self.__getattribute__(xs[0])(xs)
+
+    def print(self,xs):
+        if len(xs) == 1:
+            print()
+
+        l = len(xs)-1
+        for i,x in enumerate(xs[1:]):
+            e=self.eval(x)
+            if i<l-1:
+                print(e,end="") 
+            else:
+                if e!=",":
+                    print(e)
+                else:
+                    print(e,end="")    
+code=[
+    ["Print","Hello World!","Sky is blue"],
+    ["Print",1,","],
+    ["Print",2,","],
+    ["Print",3],
+    ["Print","a"],
+    ["Print","b"],
+    ["Print"],
+    ["Print","c"],
+]
+
+interpreter=Interpreter()
+
+interpreter.run(code)
